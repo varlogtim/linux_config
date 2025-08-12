dafa94578603eb5358a967301e178b0771ccef1a
Tim Tucker <varlogtim@gmail.com>
2025-08-12 16:05:39 -0400

    Random Updates

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -79,7 +79,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 -- beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
-beautiful.init("/home/ttucker/thinkpad/awesome/theme.lua")
+beautiful.init("/home/ttucker/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
