03c8fbd761edec97bddbc8dfda5f846ea5b95fb3
Tim Tucker <varlogtim@gmail.com>
2025-09-03 10:30:29 -0400

    Bashrc and TODO list updates
    - TODO list updates
    - Small bashrc restructuring
    - title bar color change

--- bashrcs/main.bashrc.sh
+++ bashrcs/main.bashrc.sh
@@ -1,17 +1,13 @@
 # Emacs mode
 set -o emacs
 
-BASHRC_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
-# Prompt
-if [ -f "${BASHRC_DIR}/prompt.bashrc.sh" ]; then
-    source "${BASHRC_DIR}/prompt.bashrc.sh"
-fi
-
-# Work Aliases
-if [ -f "${BASHRC_DIR}/work.bashrc.sh" ]; then
-    source "${BASHRC_DIR}/work.bashrc.sh"
-fi
+export TZ='America/New_York'
+export XDG_CURRENT_DESKTOP=sway
+export BROWSER="/usr/bin/firefox"
+export CSCOPE_EDITOR=/usr/bin/vim
+export EDITOR=/usr/bin/vim
+export TERMINFO="/usr/share/terminfo"
 
 # Aliases
 alias ls='ls --color=always'
@@ -27,20 +23,7 @@ alias topmem='top -bn1 -o%MEM | head -n 20'
 
 # TODO:
 # - Break specific components out into separate bashrc files and/or functions.
-# - Figure out a sway bind to launch different terminals with different vars evaluated.
-#   - The use case here is that I want a key bind for my work dev environment.
-
-# DirEnv
-if [ -x "$(command -v direnv)" ]; then
-    eval "$(direnv hook bash)"
-fi
-
-# Browser
-export BROWSER="/usr/bin/firefox"
-
-# Editors
-export CSCOPE_EDITOR=/usr/bin/vim
-export EDITOR=/usr/bin/vim
+# - Create a script which populates direnv files with specific versions of utils.
 
 
 # Include paths
@@ -48,11 +31,11 @@ export LD_LIBRARY_PATH=/usr/lib/modules/$(uname -r)/build/include/:/usr/lib/modu
 export LIBRARY_PATH=/usr/lib/modules/$(uname -r)/build/include/:/usr/lib/modules/$(uname -r)/build/arch/x86/include/:/usr/lib/modules/$(uname -r)/build/arch/x86/include/generated/:/usr/include/
 export C_INCLUDE=/usr/lib/modules/$(uname -r)/build/include/:/usr/lib/modules/$(uname -r)/build/arch/x86/include/:/usr/lib/modules/$(uname -r)/build/arch/x86/include/generated/:/usr/include/
 
-# FZF
-# Enable fzf key bindings
-source /usr/share/fzf/key-bindings.bash
-# Enable fzf completion
-source /usr/share/fzf/completion.bash
+
+
+##########################################
+# Global Environment Setup
+#
 
 # bash-completion
 BASH_COMPLETION='/usr/share/bash-completion/bash_completion'
@@ -60,31 +43,66 @@ if [ -e ${BASH_COMPLETION} ]; then
     source ${BASH_COMPLETION}
 fi
 
+# Fuzzy Finder
+if [ -e /usr/share/fzf/key-bindings.bash ]; then
+    source /usr/share/fzf/key-bindings.bash
+fi
+if [ -e /usr/share/fzf/completion.bash ]; then
+    source /usr/share/fzf/completion.bash
+fi
+
+# Direnv
+if [ -x "$(command -v direnv)" ]; then
+    eval "$(direnv hook bash)"
+fi
+
+# A better git log
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+
+##########################################
+# Global Language Paths
+#
+# TODO: 
+#  - setup direnvs for each, where appropriate.
+#  - create well-named symlinks, i.e., global -> somever
+#
+
 # NVM for Node.js
 # https://github.com/nvm-sh/nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-
-export TERMINFO="/usr/share/terminfo"
-# Go lang
+# Golang
 # curl -LO https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
 export GOROOT="$HOME/tmp/golang/go-1.24.1"
 export GOPATH="$HOME/tmp/golang/go_roots/go-1.24.1"
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
-# System Go
-#export PATH=$HOME/go/bin:$PATH
-
-# Paths
+# Java
 export PATH=$HOME/bin:$PATH
 export PATH=/usr/lib/jvm/java-11-openjdk/bin/:$PATH
 export PATH=$HOME/.local/bin:$PATH
 
-export TZ='America/New_York';
 
-# A better git log
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-export XDG_CURRENT_DESKTOP=sway
+##########################################
+# Extended Bashrcs:
+#
+# TODO:
+# - Add host specific bashrc
+BASHRC_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+# Prompt
+if [ -f "${BASHRC_DIR}/prompt.bashrc.sh" ]; then
+    source "${BASHRC_DIR}/prompt.bashrc.sh"
+fi
+
+# Work Aliases
+if [ -f "${BASHRC_DIR}/work.bashrc.sh" ]; then
+    source "${BASHRC_DIR}/work.bashrc.sh"
+fi
+
+
